92c46b04fb790903dd77c4e754f5bec1a2e5c061
Ravi Rai <Ravi.B.Rai@outlook.com>
2024-02-15 22:39:52 -0400

    starting to get full dataset

--- EdamamETL.py
+++ EdamamETL.py
@@ -1,28 +1,55 @@
 import requests
 import json
+import pandas as pd
 
-# Access the API keys
-with open('config.json', 'r') as f:
-    config_data = json.load(f)
+class RecipeETL:
+    def __init__(self, config_file='config.json'):
+        self.config_file = config_file
+        self.load_config()
+        #self.db_conn = None
+        
+    def load_config(self):
+        with open(self.config_file, 'r') as f:
+            self.config_data = json.load(f)
+        
+        api_keys = self.config_data.get('api_keys', {})
+        self.APP_ID = api_keys.get('edamam_app_id', None)
+        self.APP_KEY = api_keys.get('edamam_app_key', None)
 
-api_keys = config_data['api_keys']
-APP_ID = api_keys['edamam_app_id']
-APP_KEY = api_keys['edamam_app_key']
-
-def recipe_search(ingredient, from_index=0, to_index=10):
-    if to_index > 100:
-        raise ValueError("to_index must be 100 at maximum")
-    
-    app_id = APP_ID  # Replace with your Edamam API app ID
-    app_key = APP_KEY  # Replace with your Edamam API app key
-    result = requests.get(
-        'https://api.edamam.com/search?q={}&app_id={}&app_key={}&from={}&to={}'.format(
-            ingredient, app_id, app_key, from_index, to_index
+    def recipe_search(self, ingredient, from_index=0, to_index=100):
+        if to_index > 100:
+            self.logger.warning("to_index exceeds maximum value (100). Adjusting to 100.")
+            to_index = 100
+        
+        result = requests.get(
+            f'https://api.edamam.com/search?q={ingredient}&app_id={self.APP_ID}&app_key={self.APP_KEY}&from={from_index}&to={to_index}'
         )
-    )
-    data = result.json()
-    return data['hits']
+        data = result.json()
+        return data['hits']
 
-def get_recipe_df(recipes):
-    recipes_lst = [recipes[i]['recipe'] for i in range(len(recipes))]
-    return pd.DataFrame(recipes_lst)
+    def get_recipe_df(self, recipes):
+        recipes_lst = [recipe['recipe'] for recipe in recipes]
+        return pd.DataFrame(recipes_lst)
+
+    def save_to_csv(self, df, filename='recipes.csv'):
+        df.to_csv(filename, index=False)
+
+    """ implement this later - it will check if we are loading data into csv or database 
+    def run_etl(self, ingredient):
+        recipes = self.recipe_search(ingredient)
+        df = self.get_recipe_df(recipes)
+        
+        # Choose the data loading method based on configuration
+        if self.config_data.get('data_loading_method') == 'csv':
+            self.save_to_csv(df)
+        elif self.config_data.get('data_loading_method') == 'database':
+            self.save_to_database(df)
+        else:
+            raise ValueError("Unsupported data loading method specified in config file")
+    """
+
+if __name__ == "__main__":
+    etl = RecipeETL()
+    ingredient = input("Enter the ingredient: ")
+    #etl.run_etl(ingredient)
+    #etl.close_database()  # Close database connection if open
