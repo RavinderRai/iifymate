d62c3a91eaadb1521835043a1682a2ba34e3c6cf
Ravi Rai <Ravi.B.Rai@outlook.com>
2024-04-02 23:24:22 -0300

    starting Android App

---
+++ FlavourQuasarApp/app/src/main/java/com/example/flavour_quasar_app/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.flavour_quasar_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.flavour_quasar_app.ui.theme.Flavour_Quasar_AppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
