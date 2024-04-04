b7b1fd0fa68a151095c3238c07b2443e8d529c04
Ravi Rai <Ravi.B.Rai@outlook.com>
2024-04-03 22:22:23 -0300

    added drop down for health label

--- FlavourQuasarApp/app/src/main/java/com/example/flavour_quasar_app/MainActivity.kt
+++ FlavourQuasarApp/app/src/main/java/com/example/flavour_quasar_app/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.flavour_quasar_app
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,5 +20,23 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spinner: Spinner = findViewById<Spinner>(R.id.spinner)
+        val adapter: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(
+            this,
+            R.array.dropdown_items,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedItem = parent?.getItemAtPosition(position).toString()
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Handle no item selected if needed
+            }
+        }
     }
 }
