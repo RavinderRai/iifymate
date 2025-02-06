46055f4359d83c8acfe7bee2946070edfbe16d59
Ravi Rai <ravi.b.rai@outlook.com>
2025-02-06 11:35:31 -0400

    basic monitoring infra set up

--- ml_features/ml_calorie_estimation/pipeline/predict.py
+++ ml_features/ml_calorie_estimation/pipeline/predict.py
@@ -1,10 +1,11 @@
 import os
 import logging
 from fastapi import FastAPI, HTTPException
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, Response
 from pydantic import BaseModel
 from pathlib import Path
 from ml_features.ml_calorie_estimation.src.predicting.macro_predictor import MacroPredictor
+from prometheus_client import Counter, Histogram, generate_latest, CONTENT_TYPE_LATEST
 
 logger = logging.getLogger(__name__)
 
@@ -14,17 +15,28 @@ MLFLOW_TRACKING_URI = os.path.join(ML_PROJECT_ROOT, "mlruns")
 app = FastAPI()
 predictor = MacroPredictor(MLFLOW_TRACKING_URI)
 
+REQUESTS = Counter('calorie_predictions_total', 'Total number of predictions made')
+PREDICTION_TIME = Histogram('prediction_latency_seconds', 'Time spent processing prediction')
+
 class TextInput(BaseModel):
     text: str
     
 class BatchTextInput(BaseModel):
     texts: list[str]
     
+@app.get("/metrics")
+async def metrics():
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
+    
 @app.post("/predict", response_model=dict[str, float])
 async def predict(input_data: TextInput) -> JSONResponse:
     try:
-        result = predictor.predict(input_data.text)
+        # Increment requests counter
+        REQUESTS.inc()
+        with PREDICTION_TIME.time():
+            result = predictor.predict(input_data.text)
         return JSONResponse(content=result)
+    
     except Exception as e:
         logger.error(f"Error during prediction: {e}")
         raise HTTPException(status_code=500, detail=str(e))
